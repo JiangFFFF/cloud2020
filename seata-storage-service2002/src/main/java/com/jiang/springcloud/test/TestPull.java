a91c0aad9689bb15f38a36a6f5906215de823e83
JiangFFFF <1820920433@qq.com>
2022-01-11 00:15:33 +0800

    修改Test

---
+++ seata-storage-service2002/src/main/java/com/jiang/springcloud/test/TestPull.java
@@ -0,0 +1,16 @@
+package com.jiang.springcloud.test;
+
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.testng.annotations.Test;
+
+/**
+ * @author jiang
+ * @create 2022-01-11-12:06 上午
+ */
+public class TestPull {
+
+    @Test
+    public void test1(){
+        System.out.println("测试git！");
+    }
+}
